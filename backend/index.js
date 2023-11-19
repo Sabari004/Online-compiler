2d6d7b53e0fc5d6956f3855b2142fd1f19125207
Sabari Raj <sabarinagsel004@gmail.com>
2023-11-19 21:22:44 +0530

    Created unique file path

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,15 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
-  //   console.log("jfjlfajl");
-  // console.log(req.body.name);
   return res.json({ hello: "world" });
 });
 app.post("/run", (req, res) => {
-  // console.log(JSON.parse(}));
   const { language = "cpp", code } = req.body;
   if (code == undefined) {
     return res.status(400).json({ success: false, error: "Empty code body" });
   }
+  //need to generate a cpp file with content from the request
+  //we need to run the file and sent the response
   return res.json({ language, code });
 });
 app.listen(5000, () => {
