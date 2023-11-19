d0f09c3fb8b938abd4a03e36acfd49c9882e47a4
Sabari Raj <sabarinagsel004@gmail.com>
2023-11-19 21:43:33 +0530

    Saved the code from user

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -7,7 +7,7 @@ const dirCodes = path.join(__dirname, "codes");
 if (!fs.existsSync(dirCodes)) {
   fs.mkdirSync(dirCodes, { recursive: true });
 }
-const generateFile = async (format, code) => {
+const generateFile = async (format, content) => {
   const jobId = uuid();
   const filename = `${jobId}.${format}`;
   const filepath = path.join(dirCodes, filename);
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const { generateFile } = require("./generateFile");
 app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   return res.json({ hello: "world" });
 });
-app.post("/run", (req, res) => {
+app.post("/run", async (req, res) => {
   const { language = "cpp", code } = req.body;
   if (code == undefined) {
     return res.status(400).json({ success: false, error: "Empty code body" });
   }
+  const filepath = await generateFile(language, code);
   //need to generate a cpp file with content from the request
   //we need to run the file and sent the response
-  return res.json({ language, code });
+  return res.json({ filepath });
 });
 app.listen(5000, () => {
   console.log("Listening on port 5000!");
